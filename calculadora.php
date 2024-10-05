cc66a48b8d6744b66cf6568d60be5a29931e4207
McMbappe777 <146596138+McMbappe777@users.noreply.github.com>
2024-10-05 14:44:54 +0000

    add calculator to php

---
+++ calculadora.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Calculadora Full Stack </h1>
+    <form action="calculadora.php" method="post">
+        <label>Numero1</label>
+        <imput type="text" name="numero1" id="numero1"></imput>
+        <label>Numero2</label>
+        <imput type="text" name="numero2" id="numero2"></imput>
+        <imput type="submit" value="Calcular">
+    </form>
+
+    <hr/>
+    
+</body>
+</html>
